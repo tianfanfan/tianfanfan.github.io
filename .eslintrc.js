88c0fb6e9ded0d8ab7bee4d698b1155082a59ab0
tianfanfan <tianfanfan7@gmail.com>
2018-12-31 12:47:33 +0800

    [feat] update

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,27 +1,15 @@
 module.exports = {
-    "env": {
-        "browser": true
+    env: {
+        browser: true
     },
-    "extends": "eslint:recommended",
-    "parserOptions": {
-        "ecmaVersion": 5
+    extends: 'eslint:recommended',
+    parserOptions: {
+        ecmaVersion: 5
     },
-    "rules": {
-        "indent": [
-            "error",
-            4
-        ],
-        "linebreak-style": [
-            "error",
-            "windows"
-        ],
-        "quotes": [
-            "error",
-            "single"
-        ],
-        "semi": [
-            "error",
-            "never"
-        ]
+    rules: {
+        indent: ['error', 2],
+        'linebreak-style': ['error', 'windows'],
+        quotes: ['error', 'single'],
+        semi: ['error', 'never']
     }
-};
+}
