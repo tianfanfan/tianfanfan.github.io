2f7e31d17e1bb381569c492abc5bbd76a2d786a6
tianfanfan <910520150@qq.com>
2019-01-28 14:49:41 +0800

    [feat] 添加第二个滑动 demo

---
+++ vue-recycle-demo-2/precache-manifest.bcde0c2d5d9280670a08f74665444a81.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/vue-recycle-demo-2/robots.txt"
+  },
+  {
+    "revision": "9876f4beaab5e1a5ff18",
+    "url": "/vue-recycle-demo-2/js/chunk-vendors.042b3a3d.js"
+  },
+  {
+    "revision": "09eda98cd9170a460387",
+    "url": "/vue-recycle-demo-2/js/app.1e6e196f.js"
+  },
+  {
+    "revision": "dfd0e22895c84119220f",
+    "url": "/vue-recycle-demo-2/js/about.b1dedf28.js"
+  },
+  {
+    "revision": "b3b4a15780311864f3c97adce0ce5c6f",
+    "url": "/vue-recycle-demo-2/index.html"
+  },
+  {
+    "revision": "9876f4beaab5e1a5ff18",
+    "url": "/vue-recycle-demo-2/css/chunk-vendors.5ad2fd9c.css"
+  },
+  {
+    "revision": "09eda98cd9170a460387",
+    "url": "/vue-recycle-demo-2/css/app.4949ce06.css"
+  }
+];
