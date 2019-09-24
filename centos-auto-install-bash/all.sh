792a9c3b95761d21b60db29c6a465ba46c9f93b3
tianfanfan <admin@tianfanfan.com>
2019-09-23 23:49:41 -0400

    feature add all shell

---
+++ centos-auto-install-bash/all.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+yum -y install wget
+
+wget https://raw.githubusercontent.com/tianfanfan/nginx--install-shell/master/nginx.sh && bash nginx.sh
+
+wget https://raw.githubusercontent.com/tianfanfan/tianfanfan.github.io/master/centos-auto-install-bash/ss.sh && bash ss.sh
+
+wget https://raw.githubusercontent.com/tianfanfan/tianfanfan.github.io/master/centos-auto-install-bash/git-install2.sh && bash git-install2.sh
+
