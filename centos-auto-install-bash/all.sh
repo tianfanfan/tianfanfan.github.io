0ccd4cb13153a2ec20a994edf86be03c600a4fc4
tianfanfan <tffahyq@qq.com>
2019-09-24 12:45:00 +0800

    Update all.sh

--- centos-auto-install-bash/all.sh
+++ centos-auto-install-bash/all.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 yum -y install wget
-
+mkdir centos-auto-install-bash
+cd centos-auto-install-bash
 wget https://raw.githubusercontent.com/tianfanfan/nginx--install-shell/master/nginx.sh && bash nginx.sh
 
 wget https://raw.githubusercontent.com/tianfanfan/tianfanfan.github.io/master/centos-auto-install-bash/ss.sh && bash ss.sh
 
-wget https://raw.githubusercontent.com/tianfanfan/tianfanfan.github.io/master/centos-auto-install-bash/git-install2.sh && bash git-install2.sh
+wget https://raw.githubusercontent.com/tianfanfan/tianfanfan.github.io/master/centos-auto-install-bash/git-install.sh && bash git-install.sh
 
