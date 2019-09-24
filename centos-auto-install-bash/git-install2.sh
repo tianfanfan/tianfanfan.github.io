fe70cedf1236419db0edee3ec1cc1c796a124ed3
tianfanfan <tffahyq@qq.com>
2019-09-24 11:47:20 +0800

    Update git-install2.sh

--- centos-auto-install-bash/git-install2.sh
+++ centos-auto-install-bash/git-install2.sh
@@ -1,3 +1,6 @@
+# yum install http://opensource.wandisco.com/centos/6/git/x86_64/wandisco-git-release-6-1.noarch.rpm
+# yum install http://opensource.wandisco.com/centos/7/git/x86_64/wandisco-git-release-7-1.noarch.rpm
+# yum install http://opensource.wandisco.com/centos/7/git/x86_64/wandisco-git-release-7-2.noarch.rpm
 yum install -y http://opensource.wandisco.com/centos/7/git/x86_64/wandisco-git-release-7-2.noarch.rpm
 yum install -y git
 git --version
